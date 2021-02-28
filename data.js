ef08fac206d95d109aa6c26fa95213b2817ac802
Daniel Vasquez <danielvasquez@Mac-mini-de-Daniel.local>
2021-02-28 17:16:24 -0600

    Primer commit

---
+++ data.js
@@ -0,0 +1,76 @@
+const flowers = [
+    {
+      id: 1,
+      name: "Girasol.",
+      description: "¿Qué es el girasol? \n Helianthus annus o, lo que es lo mismo, el girasol, es una de plantas con flor más populares. Originaria de América, en concreto del centro y el norte de este continente, es una de las más cultivadas para el sector de la alimentación, del aceite y para la ornamentación de jardines.",
+      image: "https://heraldodemexico.com.mx/wp-content/uploads/2020/07/girasol-signficado-regalar-flor.jpg"
+    },
+    {
+      id: 2,
+      name: "Hortensia.",
+      description: "El género Hydrangea incluye plantas ornamentales, comúnmente conocidas como hortensias, nativas del sur y el este de Asia2​ (concretamente de China, Japón, Corea, el Himalaya e Indonesia) y de América.",
+      image: "https://s1.best-wallpaper.net/wallpaper/m/1708/Hydrangea-flowers-pink-petals-close-up_m.jpg"
+    },
+    {
+      id: 3,
+      name: "Tulipanes.",
+      description: "Tulipa es un género de plantas perennes y bulbosas perteneciente a la familia Liliaceae, en el que se incluyen los populares tulipanes, nombre común con el que se designa a todas las especies, híbridos y cultivares de este género.",
+      image: "https://besthqwallpapers.com/Uploads/20-4-2018/49186/thumb2-colorful-tulips-4k-blue-sky-spring-sunny-day.jpg"
+    },
+    {
+      id: 4,
+      name: "Dalias.",
+      description: "El primer nombre dado a esta flor en náhuatl fue atlcotlixochitl, que viene de: a-tl (agua), coco-tli (tubo) y xochitl (flor). Una especie de dalia en particular fue la flor originaria de Aztlán.",
+      image: "https://besthqwallpapers.com/Uploads/18-8-2019/101542/thumb2-pink-dahlia-4k-bokeh-pink-flowers-close-up.jpg"
+    },
+    {
+      id: 5,
+      name: "Lirios.",
+      description: "Por lirio se hace referencia a las plantas del género Iris y otros géneros similares de la misma familia de las iridáceas.",
+      image: "https://p4.wallpaperbetter.com/wallpaper/201/543/701/summer-flowers-lily-spring-wallpaper-preview.jpg"
+    },
+    {
+      id: 6,
+      name: "Grandiflora.",
+      description: "Grandiflora (palabra en latín para <flor grande>) es un grupo de rosas modernas de jardín que fue desarrollado por el cruce de híbrido de té con rosas floribunda. \n La grandiflora posee características tanto de la híbrida de té como de la floribunda.",
+      image: "https://i.pinimg.com/originals/36/bd/e3/36bde3e3bfe8f36ddd2a1d559ca4c793.jpg"
+    },
+    {
+      id: 7,
+      name: "freedom.",
+      description: "La rosa roja Freedom es la rosa roja más utilizada en el mundo floral (junto con la Red Naomi). \n Desarrollada por Tantau, desde su aparición ha sido la reina en todos los cultivos de Colombia y Ecuador. Exportada, literalmente, a todo el mundo, su belleza y calidad la han convertido en una rosa dificilmente de superar. ",
+      image: "https://i.pinimg.com/originals/18/f5/3a/18f53a2162ea2feca7ff02c7b8a6e98d.jpg"
+    },
+    {
+      id: 8,
+      name: "Flor de Loto.",
+      description: "Flor de loto, también conocida como loto sagrado, loto indio o rosa del Nilo, es el nombre común de una planta acuática llamada Nelumbo nucifera, la cual se caracteriza por tener hojas flotantes y olorosas, además de un fruto de estructura compleja con múltiples hoyos que asemejan pequeños ojos.",
+      image: "https://besthqwallpapers.com/Uploads/29-1-2019/78874/thumb2-4k-pink-lotus-macro-bokeh-pink-flowers.jpg"
+    },
+    {
+      id: 9,
+      name: "Ave del Paraíso.",
+      description: "Strelitzia reginae, llamada popularmente ave del paraíso, es una especie herbácea originaria de Sudáfrica. Se cultiva extensamente como planta ornamental por la forma peculiar de su flor.",
+      image: "https://p4.wallpaperbetter.com/wallpaper/689/45/88/bird-of-paradise-hd-wallpaper-preview.jpg"
+    },
+    {
+      id: 10,
+      name: "Caléndulas.",
+      description: "Calendula, llamado comúnmente caléndula o margarita, es un género de entre 12 a 20 especies de hierbas anuales o perennes de la familia de las asteráceas, nativas de la región mediterránea y el Asia Menor.",
+      image: "https://4kwallpapers.com/images/walls/thumbs/1681.jpg"
+    },
+    {
+      id: 11,
+      name: "Camelias",
+      description: "La Camelia llegó a Europa hace más de 4 siglos, procedente del continente asiático. \n Las Camelias son tan fieles a la exhibición de su belleza que sus tallos con hojas de verde intenso, son capaces de florecer incluso aun cuando se asoma el período invernal.",
+      image: "https://hddesktopwallpapers.in/wp-content/uploads/2015/08/pink-camellia-hd.jpg"
+    },
+    {
+      id: 12,
+      name: "Orquídeas",
+      description: "Las orquídeas han fascinado al mundo durante siglos debido a su belleza y sus muchas características que las hacen únicas en el reino vegetal. Están presentes en casi todo el mundo, aunque se desarrollan preferentemente en los trópicos.",
+      image: "https://i.pinimg.com/originals/e8/1e/af/e81eaf71c9052171b2dbc2fe33d83bc9.jpg"
+    },
+  ]
+  
+  export default flowers;
---
+++ index.js
@@ -0,0 +1,51 @@
+import flowers from "./data.js";
+
+const apiFlores = document.getElementById("api-flores");
+const foto_flor = document.getElementById("foto");
+const name_flor = document.getElementById("name");
+const descripcion = document.getElementById("description");
+
+function pintarFlores(flowers) {
+  apiFlores.innerHTML = "";
+
+  flowers.forEach((flower) => {
+    const li = document.createElement("li");
+    li.className = "style-li";
+    li.style = "margin: 5px; padding: 5px;";
+    const button = document.createElement("button");
+
+    button.innerText = "Ver";
+    button.className =
+      "button-ver";
+    button.style =
+      "margin-left: 5px; width: 50px; height: 30px; border-radius: 5px; color: white;";
+
+    button.addEventListener("click", () => {
+      console.log(flower);
+      pintarFlor(flower);
+    });
+
+    li.innerHTML = flower.name;
+    li.appendChild(button);
+
+    apiFlores.appendChild(li);
+  });
+}
+
+function pintarFlor(flowers) {
+  const { image, name, description } = flowers || {};
+
+  foto_flor.src =
+    image ||
+    "https://astelus.com/wp-content/viajes/Pa%C3%ADs-de-origen-de-las-flores-m%C3%A1s-bonitas-del-mundo.jpg";
+  name_flor.innerText = name || "Sin nombre...";
+  descripcion.innerText = description || "Sin descripción...";
+  foto_flor.style = "border-radius: 10px;";
+}
+
+function init() {
+  pintarFlores(flowers);
+  pintarFlor();
+}
+
+init();
